0526ffaeb3cc3a0eb9eee5e66ef086059427cf71
namu2267 <namu2267@gmail.com>
2024-02-04 11:25:27 +0900

    chore:파일구조정리 및 기타

---
+++ src/app/summary/[storename]/page.tsx
@@ -0,0 +1,11 @@
+const Page = async ({ params }: { params?: { storename: string } }) => {
+  const storeParams = params?.storename;
+
+  return (
+    <div className="flex flex-col items-center bg-white fixed top-0 left-0 overflow-auto w-2/6 h-full">
+      <p>서머리페이지</p>
+    </div>
+  );
+};
+
+export default Page;

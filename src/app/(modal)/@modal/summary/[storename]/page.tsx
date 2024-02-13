e0d3a109b92465be804d53616bf40354fb38407c
namu2267 <namu2267@gmail.com>
2024-02-13 16:21:19 +0900

    feat:summary페이지 레이아웃 정리

--- src/app/(modal)/@modal/summary/[storename]/page.tsx
+++ src/app/(modal)/@modal/summary/[storename]/page.tsx
@@ -47,11 +47,13 @@ const Page = async ({ params }: { params?: { storename: string } }) => {
   const { data } = await res.json();
   const storeData = data as MainDB;
 
+  console.log(storeData);
+
   const updateData = new Date(storeData.updatedAt).toISOString().slice(0, 10);
 
   return (
     <div className="flex flex-col bg-white absolute top-0 left-0 overflow-auto w-2/6 h-full">
-      <div className="my-10">
+      <div className="my-10 relative">
         <div className="my-10">
           <h1 className="text-3xl text-center">{storeData.name}</h1>
           <p className=" text-center text-gray-400">{storeData.category}</p>
@@ -60,27 +62,32 @@ const Page = async ({ params }: { params?: { storename: string } }) => {
           <Image
             key={storeData.storeImages[0].photoId}
             src={storeData.storeImages[0].file_path}
-            alt={storeData.storeImages[0] + "메인사진"}
+            alt={storeData.engName + "메인사진"}
             width={0}
             height={0}
             fill={true}
             sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
             style={{ objectFit: "cover" }}
             priority={true}
-            className="z-1"
-            // unoptimized={true}
           />
         </div>
-        <main className="my-10 border-b-2 border-gray-200 	border-style: solid">
-          <div className="flex items-center">
+
+        <nav className="bg-black text-white text-center p-3 font-bold">
+          <ul className="flex justify-center gap-x-20">
+            <li>길안내</li>
+            <li>공유</li>
+          </ul>
+        </nav>
+        <main className="p-10 border-b-2 border-gray-300 	border-style: solid">
+          <div className="flex items-center m-3">
             <div className="w-5 h-5 bg-logo-location bg-no-repeat bg-cover bg-center"></div>
-            <p>
+            <p className="ml-2">
               {storeData.address} {storeData.detailAddress}
             </p>
           </div>
-          <div className="flex items-center">
+          <div className="flex items-center m-3">
             <div className="w-5 h-5 bg-logo-subway bg-no-repeat bg-cover bg-center"></div>
-            <p>
+            <p className="ml-2">
               {storeData.nearestRoute?.subwayLine &&
                 (storeData.nearestRoute.subwayLine.length === 1
                   ? storeData.nearestRoute.subwayLine[0]
@@ -88,40 +95,81 @@ const Page = async ({ params }: { params?: { storename: string } }) => {
               호선, {storeData.nearestRoute?.routeInfo}
             </p>
           </div>
-          <div className="flex items-center">
-            <div className="w-5 h-5 bg-logo-time bg-no-repeat bg-cover bg-center"></div>
+          <div className="flex items-center flex-col relative m-3">
+            <div className="w-5 h-5 bg-logo-time bg-no-repeat bg-cover bg-center absolute left-0"></div>
             {storeData.operationTime.map((operation) => {
               return (
-                <p key={operation.day}>
-                  {operation.day}
-                  {operation.startTime && operation.endTime
-                    ? `${operation.startTime} open ${operation.endTime} close`
-                    : "휴무"}
-                </p>
+                <div
+                  className="ml-2 flex flex-col"
+                  key={storeData.engName + operation.day}
+                >
+                  <div className="flex">
+                    <span className="font-semibold mr-2">{operation.day}</span>
+                    <p>
+                      {operation.startTime && operation.endTime
+                        ? `${operation.startTime} - ${operation.endTime} `
+                        : "휴무"}
+                    </p>
+                  </div>
+                </div>
               );
             })}
           </div>
-          <div className="flex items-center">
+          <div className="flex items-center m-3">
             <div className="w-5 h-5 bg-logo-instagram bg-no-repeat bg-cover bg-center"></div>
-            <p>{storeData.socialLinks}</p>
+            <p className="ml-2">{storeData.socialLinks}</p>
           </div>
-          <div className="flex">
+          <div className="flex items-center m-3">
             <div className="w-5 h-5 bg-logo-phone bg-no-repeat bg-cover bg-center"></div>
-            <p>{storeData.phoneNumber}</p>
+            <p className="ml-2">{storeData.phoneNumber}</p>
           </div>
         </main>
-        <div className="absolute bottom-0 right-0">
-          <p>{updateData}</p>
+        <div className="bg-black text-white text-center p-3 font-bold">
+          <Link href={`detail/${storeParams}`}>상세정보보기</Link>
         </div>
-
-        <Link href={`detail/${storeParams}`}>
-          <button>디테일 페이지 이동버튼</button>
-        </Link>
+        <header className="p-8 bg-white fixed top-0 left-0 w-2/6 h-5 z-100">
+          <h1 className="text-4xl font-bold ">IYO</h1>
+        </header>
       </div>
 
-      <header className="p-8 bg-white fixed top-0 left-0 w-2/6 h-5 z-100">
-        <h1 className="text-4xl font-bold ">IYO</h1>
-      </header>
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente,
+        soluta excepturi. Sunt neque consectetur minus facere libero enim.
+        Voluptatem, omnis. In a mollitia et delectus laborum illum aut
+        consequuntur impedit. Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Beatae enim soluta blanditiis fugit possimus odio inventore
+        repellendus, maxime ratione labore consequatur suscipit ipsam quis
+        deleniti ad facilis dolore. Sint, dicta! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Ullam repellendus accusamus voluptatibus
+        debitis harum eveniet quis dolorem quisquam, recusandae, modi sed culpa
+        temporibus magnam veniam porro eius mollitia? Numquam, deleniti! Lorem
+        ipsum dolor, sit amet consectetur adipisicing elit. Sapiente, soluta
+        excepturi. Sunt neque consectetur minus facere libero enim. Voluptatem,
+        omnis. In a mollitia et delectus laborum illum aut consequuntur impedit.
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae enim
+        soluta blanditiis fugit possimus odio inventore repellendus, maxime
+        ratione labore consequatur suscipit ipsam quis deleniti ad facilis
+        dolore. Sint, dicta! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Ullam repellendus accusamus voluptatibus debitis harum eveniet
+        quis dolorem quisquam, recusandae, modi sed culpa temporibus magnam
+        veniam porro eius mollitia? Numquam, deleniti! Lorem ipsum dolor, sit
+        amet consectetur adipisicing elit. Sapiente, soluta excepturi. Sunt
+        neque consectetur minus facere libero enim. Voluptatem, omnis. In a
+        mollitia et delectus laborum illum aut consequuntur impedit. Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Beatae enim soluta
+        blanditiis fugit possimus odio inventore repellendus, maxime ratione
+        labore consequatur suscipit ipsam quis deleniti ad facilis dolore. Sint,
+        dicta! Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam
+        repellendus accusamus voluptatibus debitis harum eveniet quis dolorem
+        quisquam, recusandae, modi sed culpa temporibus magnam veniam porro eius
+        mollitia? Numquam, deleniti!
+      </p>
+      <footer className="relative">
+        <div className="mx-2 mt-10 absolute right-0 bottom-0">
+          <span className="text-gray-300 mr-4">정보업데이트</span>
+          <span className="text-gray-300">{updateData}</span>
+        </div>
+      </footer>
     </div>
   );
 };

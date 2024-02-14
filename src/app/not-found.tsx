b662bfac4fcfcaa437cabb7fdffaeab44466cbd1
namu2267 <namu2267@gmail.com>
2024-02-14 19:33:17 +0900

    not-found 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div>
+      <h2>Not Found</h2>
+      <p>페이지를 찾을 수 없습니다.</p>
+      <Link href="/">Return Home</Link>
+    </div>
+  );
+}

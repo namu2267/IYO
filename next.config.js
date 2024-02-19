dd58a8dcc1114b2ba6c8d3dc11924bc6bfff2ea8
namu2267 <namu2267@gmail.com>
2024-02-19 21:15:19 +0900

    add:svg 추가

--- next.config.js
+++ next.config.js
@@ -3,7 +3,13 @@ const nextConfig = {
   reactStrictMode: false,
   images: {
     domains: ["d3uzax8st0176c.cloudfront.net"],
-    unoptimized: true,
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
   },
 };
 

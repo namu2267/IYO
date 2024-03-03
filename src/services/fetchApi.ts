8e2f51d34c18d58450e581078c4b8f737aaa9b7b
namu2267 <namu2267@gmail.com>
2024-03-03 20:15:10 +0900

    fix:fetch API주소변경, summary의 page.tsx metadataBase주소변경

--- src/services/fetchApi.ts
+++ src/services/fetchApi.ts
@@ -1,7 +1,7 @@
 import { MainDB } from "@/types/storeTypes";
 
 export const getMainDb = async (storeParams?: string) => {
-  const res = await fetch(`http://localhost:4000/detail/${storeParams}`, {
+  const res = await fetch(`https://new-todos.site/detail/${storeParams}`, {
     method: "GET",
     headers: {
       "Content-Type": "application/json",
